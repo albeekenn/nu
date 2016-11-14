f160ca02f5bbec8f052004dc3865d5313764922d
albeekenn <albeekenn@outlook.com>
2016-11-13 17:35:19 -0800

    Update start_apex.py

--- start_apex.py
+++ start_apex.py
@@ -1,3 +1,3 @@
 #! /usr/bin/python
 
-nohup java -Dapex.home=./apex -Dapex.images=/home/apexuser/apex/images -Dapex.erase -jar ./apex.war&
+nohup java -Dapex.home=./apex -Dapex.images=/home/apexuser/apex/images -Dapex.erase -jar ./apex.war > apxlsnr.log &
