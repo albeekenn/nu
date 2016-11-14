967ae3642232529261b154c871a86d9a2726cc12
albeekenn <albeekenn@outlook.com>
2016-11-13 17:27:06 -0800

    added nohup

--- start_apex.py
+++ start_apex.py
@@ -1,3 +1,3 @@
 #! /usr/bin/python
 
-java -Dapex.home=./apex -Dapex.images=/home/apexuser/apex/images -Dapex.erase -jar ./apex.war&
+nohup java -Dapex.home=./apex -Dapex.images=/home/apexuser/apex/images -Dapex.erase -jar ./apex.war&
